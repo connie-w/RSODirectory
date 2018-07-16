1161ee273a6e317608893765aff2c6e4dfae819c
roxolani-kisho <33471929+Roxolani-Kisho@users.noreply.github.com>
2018-07-15 21:36:00 -0700

    info is in a nested list

---
+++ datascrape/convert.py
@@ -0,0 +1,17 @@
+import csv
+
+all_clubs = []
+#indiviudal club info is stored in a python list with each club attached to all_clubs of type list
+with open("rso-data.csv",'r') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)
+    for row in reader:
+        name = row.pop(1)
+        description = row.pop(1)
+        email = row.pop(1)
+        category = row.pop(1) 
+        logo = row.pop(1) 
+        club = [name,description,email,category,logo]
+        all_clubs.append(club)
+        
+print(len(all_clubs))
